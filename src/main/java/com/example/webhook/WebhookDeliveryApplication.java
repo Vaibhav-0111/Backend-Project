7f333abc78e70129f6dff6499f862c4745e75c63
Vaibhav-0111 <vaibhavtripathi724@gmail.com>
2026-08-17 12:13:38 +0530

    chore: project skeleton, docker compose, env template

---
+++ src/main/java/com/example/webhook/WebhookDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookDeliveryApplication.class, args);
+	}
+
+}
